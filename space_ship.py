a365d7a7a1dc4a7dc9eae20468bf54ae762d14d1
Winlandiano <winlandiano@gmail.com>
2014-11-07 16:42:24 +0800

    add mistle with sound, add thrust sound, adjust thrust acceleration, friction and mistle speed.

--- space_ship.py
+++ space_ship.py
@@ -12,8 +12,9 @@ lives = 3
 time = 0.5
 acceleration = 0.3
 rotate_speed = 0.1
-retard_ratio = 0.1
+retard_ratio = 0.04
 friction_ratio = 0.02
+missile_speed = 10
 
 class ImageInfo:
     def __init__(self, center, size, radius = 0, lifespan = None, animated = False):
@@ -61,6 +62,9 @@ splash_image = simplegui.load_image("http://commondatastorage.googleapis.com/cod
 ship_info = ImageInfo([45, 45], [90, 90], 35)
 ship_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/double_ship.png")
 
+# ship thrusting
+ship_trust_info = ImageInfo([135, 45], [90, 90], 35)
+
 # missile image - shot1.png, shot2.png, shot3.png
 missile_info = ImageInfo([5,5], [10, 10], 3, 50)
 missile_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/shot2.png")
@@ -102,6 +106,7 @@ class Ship:
         self.image_center = info.get_center()
         self.image_size = info.get_size()
         self.radius = info.get_radius()
+        self.info = info
         
     def draw(self,canvas):
         canvas.draw_image(self.image, self.image_center, self.image_size, self.pos, self.image_size, self.angle)
@@ -114,17 +119,25 @@ class Ship:
         global retard_ratio
         global WIDTH
         global HEIGHT
+        global ship_trust_info
+        ship_thrust_sound
 
         if self.thrust:
             forward = angle_to_vector(self.angle)
             self.vel[0] = (1-friction_ratio)*self.vel[0] + acceleration*forward[0]
             self.vel[1] = (1-friction_ratio)*self.vel[1] + acceleration*forward[1]
+            self.image_center = ship_trust_info.get_center()
+            self.image_size = ship_trust_info.get_size()
         elif self.retard:
             self.vel[0] = self.vel[0]*(1-friction_ratio-retard_ratio)
             self.vel[1] = self.vel[1]*(1-friction_ratio-retard_ratio)
+            self.image_center = self.info.get_center()
+            self.image_size = self.info.get_size()
         else:
             self.vel[0] = self.vel[0]*(1-friction_ratio)
             self.vel[1] = self.vel[1]*(1-friction_ratio)
+            self.image_center = self.info.get_center()
+            self.image_size = self.info.get_size()
 
         self.angle += self.angle_vel
         self.pos[0] = (self.pos[0]+self.vel[0]+WIDTH)%WIDTH
@@ -146,17 +159,17 @@ class Sprite:
         self.lifespan = info.get_lifespan()
         self.animated = info.get_animated()
         self.age = 0
-        if sound:
-            sound.rewind()
-            sound.play()
+        # if sound:
+        #     sound.rewind()
+        #     sound.play()
    
     def draw(self, canvas):
         canvas.draw_image(self.image, self.image_center, self.image_size, self.pos, self.image_size, self.angle)
     
     def update(self):
         self.angle += self.angle_vel
-        # self.pos[0]+=self.vel[0]
-        # self.pos[1]+=self.vel[1]
+        self.pos[0] = (self.pos[0]+self.vel[0]+WIDTH)%WIDTH
+        self.pos[1] = (self.pos[1]+self.vel[1]+WIDTH)%WIDTH
 
            
 def draw(canvas):
@@ -186,10 +199,18 @@ def rock_spawner():
     x = random.randint(0,WIDTH+1)
     y = random.randint(0,HEIGHT+1)
     a_rock.pos = [x,y]
+    a_rock.angle_vel = random.random()*0.4-0.2
+    a_rock.vel[0] = random.random()*3-1.5
+    a_rock.vel[1] = random.random()*3-1.5
+
 
 def keydown_handler(key):
+    global missile_speed
     if chr(key)=='&':
         #up
+        if not my_ship.thrust:
+            ship_thrust_sound.rewind()
+            ship_thrust_sound.play()
         my_ship.thrust = True
     elif chr(key)=='(':
         #down
@@ -201,14 +222,22 @@ def keydown_handler(key):
         #right
         my_ship.angle_vel = 0.1
     elif chr(key)==' ':
-        #space TODO
-        pass
+        #space
+        forward = angle_to_vector(my_ship.angle)
+        a_missile.pos[0] = my_ship.pos[0]+45*forward[0]
+        a_missile.pos[1] = my_ship.pos[1]+45*forward[1]
+        a_missile.vel[0] = my_ship.vel[0]+missile_speed*forward[0]
+        a_missile.vel[1] = my_ship.vel[1]+missile_speed*forward[1]
+        missile_sound.rewind()
+        missile_sound.play()
     else:
         pass
     
 def keyup_handler(key):
     if chr(key)=='&':
         #up
+        if my_ship.thrust:
+            ship_thrust_sound.pause()
         my_ship.thrust = False
     elif chr(key)=='(':
         #down
